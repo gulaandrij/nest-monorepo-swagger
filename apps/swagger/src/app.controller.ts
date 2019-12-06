50415a1ebbb2e3988e6a89e5cdecbf872b706563
gylaandrij <gyla.andrij@gmail.com>
2019-12-06 16:04:37 +0200

    file upload

--- apps/swagger/src/app.controller.ts
+++ apps/swagger/src/app.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TestDto } from '@app/test/test.dto';
+import { ApiResponse } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiImplicitFile } from '@nestjs/swagger/dist/decorators/api-implicit-file.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
+  @ApiResponse({status: 200})
   getHello(@Body()test: TestDto): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @ApiImplicitFile({
+    name: 'file',
+    description: '',
+  })
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile('file') file) {
+    console.log(file);
   }
 }
--- apps/swagger/src/app.module.ts
+++ apps/swagger/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
 
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      storage: multer.diskStorage({
+        destination: 'uploads',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
